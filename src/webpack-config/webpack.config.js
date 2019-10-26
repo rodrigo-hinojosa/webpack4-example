889d69d3a55c345cd9d9e4a7b937505ed4f6ee8a
Rodrigo Hinojosa <rodrigo.hinojosaacuna@cencosud.cl>
2019-10-26 19:15:50 -0300

    changed base dir

--- src/webpack-config/webpack.config.js
+++ src/webpack-config/webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
-const APP_DIR = path.resolve(__dirname, '../app');
+const APP_BASE_DIR = path.resolve(__dirname, '../../');
+const APP_DIR_SRC = path.resolve(__dirname, '../../src');
 const WebpackDashboard = require('webpack-dashboard/plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const TsConfigPathsWebpackPlugin = require('tsconfig-paths-webpack-plugin');
@@ -7,36 +8,36 @@ const FixStyleOnlyEntriesPlugin = require('webpack-fix-style-only-entries');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-    context: APP_DIR,
+    context: APP_BASE_DIR,
     entry: {
         app1: [
-            APP_DIR.concat('/ts/app1.ts'),
-            APP_DIR.concat('/scss/style1.scss')
+            APP_DIR_SRC.concat('/ts/app1.ts'),
+            APP_DIR_SRC.concat('/scss/style1.scss')
         ],
         app2: [
-            APP_DIR.concat('/ts/app2.ts'),
-            APP_DIR.concat('/scss/style2.scss')
+            APP_DIR_SRC.concat('/ts/app2.ts'),
+            APP_DIR_SRC.concat('/scss/style2.scss')
         ],
         app3: [
-            APP_DIR.concat('/ts/app1.ts'),
-            APP_DIR.concat('/scss/style1.scss'),
-            APP_DIR.concat('/ts/app2.ts'),
-            APP_DIR.concat('/scss/style2.scss')
+            APP_DIR_SRC.concat('/ts/app1.ts'),
+            APP_DIR_SRC.concat('/scss/style1.scss'),
+            APP_DIR_SRC.concat('/ts/app2.ts'),
+            APP_DIR_SRC.concat('/scss/style2.scss')
         ],
-        oneStyle1: APP_DIR.concat('/scss/style1.scss'),
+        oneStyle1: APP_DIR_SRC.concat('/scss/style1.scss'),
         allStyles: [
-            APP_DIR.concat('/scss/style1.scss'),
-            APP_DIR.concat('/scss/style2.scss')
+            APP_DIR_SRC.concat('/scss/style1.scss'),
+            APP_DIR_SRC.concat('/scss/style2.scss')
         ],
-        oneTs: APP_DIR.concat('/ts/app1.ts'),
+        oneTs: APP_DIR_SRC.concat('/ts/app1.ts'),
         allTs: [
-            APP_DIR.concat('/ts/app1.ts'),
-            APP_DIR.concat('/ts/app2.ts')
+            APP_DIR_SRC.concat('/ts/app1.ts'),
+            APP_DIR_SRC.concat('/ts/app2.ts')
         ],
     },
     output: {
         filename: 'js/[name].js',
-        path: APP_DIR.concat('/dist'),
+        path: APP_BASE_DIR.concat('/dist'),
         chunkFilename: '[id].js'
     },
     resolve: {
